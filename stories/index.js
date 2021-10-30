4072d9cc283ca3dcd598ba44d9f6e5417f116c0e
Japhet Elias <japhet9@gmail.com>
2021-10-30 16:23:47 -0400

    added Confirm appointment (deletion) stories

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 storiesOf("Button", module)
@@ -156,5 +157,12 @@ storiesOf("Appointment", module)
       interviewer={interviewer}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
     />
   ))
