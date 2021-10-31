f4e851b59cf86c28a8f31c585b627ffb74dc4047
Japhet Elias <japhet9@gmail.com>
2021-10-31 14:51:54 -0400

    created getInterview function that transforms a given interview object to its expected structure.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,3 @@
-
 export function getAppointmentsForDay(state, day) {
   let selectedDayAppointments = [];
   const selectedDay = state.days.find(stateDayItem => stateDayItem.name === day);
@@ -7,4 +6,19 @@ export function getAppointmentsForDay(state, day) {
     selectedDayAppointments = selectedDay.appointments.map(appointment => state.appointments[appointment]);
   }
   return selectedDayAppointments;
+}
+
+export function getInterview(state, interview) {
+  if (interview && interview.interviewer) {
+    return {
+      "student": interview.student,
+      "interviewer": {
+        "id": state.interviewers[interview.interviewer].id,
+        "name": state.interviewers[interview.interviewer].name,
+        "avatar": state.interviewers[interview.interviewer].avatar
+      }
+    };
+  } else {
+    return null;
+  }
 }
