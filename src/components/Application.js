3b9ce8c3ddd794baeb6b68aa78245a72c15dc506
Japhet Elias <japhet9@gmail.com>
2021-10-31 14:54:03 -0400

    updated so that interviewers are requested from api server and added to state, then shown in appointment.

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,13 +4,14 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   const setDay = day => setState({ ...state, day });// state object helper setter function
 
@@ -18,26 +19,36 @@ export default function Application(props) {
     // http://localhost:8001 prefix added as proxy in package.json
     const getDaysURL = '/api/days';
     const getAppointmentsURL = '/api/appointments';
-    // const getInterviewersURL = '/api/interviewers';
+    const getInterviewersURL = '/api/interviewers';
 
     Promise.all([
       axios.get(getDaysURL),
-      axios.get(getAppointmentsURL)
-      // axios.get(getInterviewersURL)
+      axios.get(getAppointmentsURL),
+      axios.get(getInterviewersURL)
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }))
     });
-    
+
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const scheduleArray = dailyAppointments.map((appointment) => (
-    <Appointment
-      key={appointment.id}
-      {...appointment}
-    />
-  ));
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -62,7 +73,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {scheduleArray}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
