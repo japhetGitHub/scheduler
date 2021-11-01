0dea07588db361e26269562465b19b1831bdc1a8
Japhet Elias <japhet9@gmail.com>
2021-10-31 23:43:23 -0400

    made function to transition back to previous mode. also added mode recall functionality via a stack.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,22 @@ import { useState } from "react";
 
 export default function useVisualMode (initialMode) {
   const [mode, setMode] = useState(initialMode);  
+  const [history, setHistory] = useState([initialMode]);
 
   function transition (next) {
+    setHistory(prev => prev = [...prev, next]);
     setMode(next);
   }
 
-  return { mode, transition };
+  function back () {
+    if (history.length > 1) {
+      setHistory(prev => {
+        prev.pop();
+        setMode(prev[prev.length - 1]);
+        return [...prev];
+      })
+    }
+  }
+
+  return { mode, transition, back };
 }
