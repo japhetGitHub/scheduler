7452302040fbc77ccb2dc2eafa27ea682f328f32
Japhet Elias <japhet9@gmail.com>
2021-11-01 01:44:33 -0400

    used the getInterviewersForDay selector to pass down an array from the Appointment component in the main application down to its Form component.

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -37,6 +37,7 @@ export default function Application(props) {
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyInterviewers = getInterviewersForDay(state, state.day);
 
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
@@ -46,10 +47,12 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={dailyInterviewers}
       />
     );
   });
 
+  
   return (
     <main className="layout">
       <section className="sidebar">
