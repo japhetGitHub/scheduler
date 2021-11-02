8f63edefa6307d96d3563d1c4d99d4c901cff5ba
Japhet Elias <japhet9@gmail.com>
2021-11-02 03:53:08 -0400

    added HTTP error handling functionality via ERROR_SAVE and ERROR_DELETE visual modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,6 +8,7 @@ import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
 import Confirm from "./Confirm";
+import Error from "./Error";
 
 import 'components/Appointment/styles.scss';
 
@@ -19,25 +20,36 @@ const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
 
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
+
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
 
   function save(name, interviewer) {
-    transition(SAVING); // show status indicator during async bookInterview save operation
-
     const interview = {
       student: name,
       interviewer
     };
-    props.bookInterview(props.id, interview).then(() => transition(SHOW)); // returns promise when axios put request is completed
+
+    transition(SAVING); // show status indicator during async bookInterview save operation
+
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch((err) => transition(ERROR_SAVE, true)); // returns promise when axios put request is responded to by api
   }
 
   function deleteInterview() {
-    transition(DELETING);
-    props.cancelInterview(props.id).then(() => transition(EMPTY));
+    transition(DELETING, true); // replaces CONFIRM prompt mode in history
+
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch((err) => transition(ERROR_DELETE, true)); // replaces DELETING mode in history
   }
 
-  function cancel() {
+  function goBack() {
     back();
   }
 
@@ -57,7 +69,7 @@ export default function Appointment(props) {
         <Form
           interviewers={props.interviewers}
           onSave={save}
-          onCancel={cancel}
+          onCancel={goBack}
         />
       )}
       {mode === SAVING && <Status message="Saving" />}
@@ -66,7 +78,7 @@ export default function Appointment(props) {
         <Confirm
           message="Are you sure you would like to delete?"
           onConfirm={deleteInterview}
-          onCancel={cancel}
+          onCancel={goBack}
         />
       )}
       {mode === EDIT && (
@@ -75,7 +87,19 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
           interviewers={props.interviewers}
           onSave={save}
-          onCancel={cancel}
+          onCancel={goBack}
+        />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error
+          message="Could not save appointment."
+          onClose={goBack}
+        />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error
+          message="Could not delete appointment."
+          onClose={goBack}
         />
       )}
     </article>
